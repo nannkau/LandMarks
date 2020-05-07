ab2a53f489e45079865fd67b0f655c1e106762e4
nannkau <48847934+nannkau@users.noreply.github.com>
2020-05-07 16:10:32 +0700

    hello hahah

--- app/src/main/java/com/example/landmarks/Main2Activity.java
+++ app/src/main/java/com/example/landmarks/Main2Activity.java
@@ -1,20 +1,44 @@
 package com.example.landmarks;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Main2Activity extends AppCompatActivity {
     private TextView txt;
+    private Button bt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         txt= (TextView) findViewById(R.id.textViewData);
-        Intent intent= getIntent();
-        String name = intent.getStringExtra("data");
+        bt2 = (Button) findViewById(R.id.button2);
+        final Intent intent= getIntent();
+        final String name = intent.getStringExtra("data");
         txt.setText(name);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url;
+                switch(name){
+                    case "Cleveland Tower City" : url="https://www.towercitycenter.com/";break;
+                    case  "Browns Football Field" :  url="https://firstenergystadium.com/";break;
+                    case  "Cleveland State University" :  url="https://www.csuohio.edu/";break;
+                    case  "Playhouse Square" : url="http://www.playhousesquare.org/";break;
+                    case    "Art Museum" : url="https://www.vietnamonline.com/attraction/fine-arts-museum-ho-chi-minh-city.html";break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + name);
+                }
+                Intent intent1=new Intent();
+                intent1.setAction(Intent.ACTION_VIEW);
+                intent1.setData(Uri.parse(url));
+                startActivity(intent1);
+            }
+        });
     }
 }
