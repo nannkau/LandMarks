782701c93613f0bd35bb072ef3937a9b54a7c0b5
nannkau <48847934+nannkau@users.noreply.github.com>
2020-05-07 12:38:15 +0700

    Nam nè

--- app/src/main/java/com/example/landmarks/MainActivity.java
+++ app/src/main/java/com/example/landmarks/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.landmarks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView landMark;
+    private ArrayList<String> data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        landMark= (ListView) findViewById(R.id.listviewLandmark);
+        data = new ArrayList<>();
+        data.add("Cleveland Tower City");
+        data.add("Browns Football Field");
+        data.add("Cleveland State University");
+        data.add("Playhouse Square");
+        data.add("Art Museum");
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_expandable_list_item_1,data);
+        landMark.setAdapter(adapter);
+        landMark.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,data.get(position),Toast.LENGTH_SHORT).show();
+            }
+
+
+
+        });
+
     }
+
 }
